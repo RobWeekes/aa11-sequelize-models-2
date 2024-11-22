411193d31cdbc65659adf3c536afa26a235a6909
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2024-11-22 14:04:17 -0800

    done with bonus

--- server/db/models/color.js
+++ server/db/models/color.js
@@ -17,28 +17,28 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
-      validate: {
-        async isUnique(value) {
-          const existingColor = await Color.findOne({ where: { name: value } });
-          if (existingColor) {
-            throw new Error('Color name must be unique');
-          }
-        }
-      }
+      unique: true
+      // validate: {
+      //   async isUnique(value) {
+      //     const existingColor = await Color.findOne({ where: { name: value } });
+      //     if (existingColor) {
+      //       throw new Error('Color name must be unique');
+      //     }
+      //   }
+      // }
     },
     isPrimary: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
-      defaultValue: false,
-      validate: {
-        isPrimaryColor(value) {
-          const primaryColors = ['Red', 'Blue', 'Yellow'];
-          if (this.name && value === true && !primaryColors.includes(this.name)) {
-            throw new Error('Only Red, Blue, and Yellow can be primary colors');
-          }
-        }
-      }
+      defaultValue: false
+      // validate: {
+      //   isPrimaryColor(value) {
+      //     const primaryColors = ['Red', 'Blue', 'Yellow'];
+      //     if (this.name && value === true && !primaryColors.includes(this.name)) {
+      //       throw new Error('Only Red, Blue, and Yellow can be primary colors');
+      //     }
+      //   }
+      // }
     }
   }, {
     sequelize,
